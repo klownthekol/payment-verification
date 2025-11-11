4210d6be4f6ac63cce8274a8b2808fa7e8785c90
klownthekol <klownthekol@gmail.com>
2025-11-10 23:37:09 -0400

    Update vercel.json pleaseeeee

--- ignore-css.js
+++ ignore-css.js
@@ -1,1 +1,7 @@
-require.extensions[".css"] = function () {};
+// Prevent Node.js from trying to parse CSS files in serverless functions
+if (require && require.extensions) {
+  require.extensions[".css"] = () => {};
+  require.extensions[".scss"] = () => {};
+  require.extensions[".less"] = () => {};
+}
+console.log("🧩 CSS imports will be ignored for Node runtime");
